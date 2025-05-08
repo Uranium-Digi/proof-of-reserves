86c24c0d5252a25da1a142bcfe9b9802d1403ec2
buranusmoon <b@uraniumdigital.com>
2025-05-08 15:45:17 -0500

    read program_id

--- transmitter/src/main.rs
+++ transmitter/src/main.rs
@@ -10,18 +10,21 @@ use crate::oracle_updater::loader::OracleUpdaterProgram;
 use anchor_client::solana_sdk::{commitment_config::CommitmentConfig, signer::Signer};
 use std::env;
 
+const DEFAULT_FEED_ID: &str = "0x000359843a543ee2fe414dc14c7e7920ef10f4372990b79d6361cdc0dd1ba782";
+
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     dotenv::dotenv().ok(); // loads .env file automatically
 
     let args: Vec<String> = env::args().collect();
-    if args.len() < 3 {
-        eprintln!("Usage: cargo run [mode: directapi|websocket] [feed_id]");
+    if args.len() < 2 {
+        eprintln!("Usage: cargo run [mode: directapi|websocket] [feed_id (optional)]");
         std::process::exit(1);
     }
 
     let mode = &args[1];
-    let feed_id = &args[2];
+    let default_feed_id = DEFAULT_FEED_ID.to_string();
+    let feed_id = args.get(2).unwrap_or(&default_feed_id);
 
     match mode.as_str() {
         "directapi" => directapi::run(feed_id).await?,
@@ -34,14 +37,8 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     let wallet = load_funding_wallet()?;
     println!("🔑 Loaded wallet pubkey: {}", wallet.pubkey());
-    // // Step 1: Load the oracle-updater program
-    // let oracle_program = OracleUpdaterProgram::new(CommitmentConfig::confirmed())?;
 
-    // // Step 2: Print the loaded program_id
-    // println!(
-    //     "✅ OracleUpdater loaded. Program ID: {}",
-    //     oracle_program.program_id
-    // );
+    let oracle_program = OracleUpdaterProgram::new(CommitmentConfig::confirmed())?;
 
     Ok(())
 }
--- transmitter/src/oracle_updater/loader.rs
+++ transmitter/src/oracle_updater/loader.rs
@@ -1,41 +1,50 @@
+use std::env;
+
 use anchor_client::{
     Client, Cluster, Program,
     solana_client::rpc_client::RpcClient,
-    solana_sdk::{commitment_config::CommitmentConfig, signature::Keypair, signer::Signer},
+    solana_sdk::{
+        commitment_config::CommitmentConfig,
+        signature::{Keypair, read_keypair_file},
+        signer::Signer,
+    },
 };
+use anchor_lang::prelude::Pubkey;
 use anyhow::Result;
+use serde_json;
 use std::rc::Rc;
 
 pub struct OracleUpdaterProgram {
-    // pub program: Program<Rc<Keypair>>,
-    // pub program_id: Pubkey,
+    pub program: Program<Rc<Keypair>>,
+    pub program_id: Pubkey,
 }
+
+use crate::wallet_loader;
 
 impl OracleUpdaterProgram {
     pub fn new(commitment: CommitmentConfig) -> Result<Self> {
-        // let rpc_url =
-        //     env::var("RPC_URL").map_err(|_| anyhow::anyhow!("RPC_URL env variable is not set"))?;
+        let rpc_url =
+            env::var("RPC_URL").map_err(|_| anyhow::anyhow!("RPC_URL env variable is not set"))?;
 
-        // let wallet = wallet_loader::load_funding_wallet()?;
+        let wallet = wallet_loader::load_funding_wallet()?;
 
-        // let provider = Client::new_with_options(
-        //     Cluster::Custom(rpc_url.to_string(), rpc_url.to_string()),
-        //     wallet,
-        //     commitment,
-        // );
+        let provider = Client::new_with_options(
+            Cluster::Custom(rpc_url.to_string(), rpc_url.to_string()),
+            wallet,
+            commitment,
+        );
 
-        // // Load program ID from the oracle-updater target directory
-        // let program_id = Pubkey::new_from_array(
-        //     std::fs::read("../../../oracle-updater/target/deploy/oracle_updater-keypair.json")?
-        //         .try_into()
-        //         .map_err(|_| anyhow::anyhow!("Invalid keypair file"))?,
-        // );
-        // println!("program_id: {}", program_id);
-        // let program = provider.program(program_id)?;
+        // Load program ID from the oracle-updater target directory
+        let keypair =
+            read_keypair_file("../oracle-updater/target/deploy/oracle_updater-keypair.json")
+                .map_err(|e| anyhow::anyhow!("Failed to read keypair file: {}", e))?;
+        let program_id = keypair.pubkey();
+        println!("program_id: {}", program_id);
+        let program = provider.program(program_id)?;
 
         Ok(Self {
-            // program,
-            // program_id,
+            program,
+            program_id,
         })
     }
 
